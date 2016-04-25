d604ab63e4a38cca8ca62f8a7b496de14b44fb69
Diego Santos Maia <diegosantosmaia@gmail.com>
2016-04-24 22:07:36 -0300

    Zabbix 3.0 Update

--- zabbix-telegram.sh
+++ zabbix-telegram.sh
@@ -3,8 +3,8 @@
 ##########################################################################
 # Zabbix-Telegram envio de alerta por Telegram com graficos dos eventos
 # Filename: zabbix-telegram.sh
-# Revision: 2.0
-# Date: 22/03/2016
+# Revision: 2.1
+# Date: 24/04/2016
 # Author: Diego Maia - diegosmaia@yahoo.com.br Telegram - @diegosmaia
 # Aproveitei algumas coisas:
 # Script getItemGraph.sh Author: Qicheng
@@ -25,23 +25,32 @@ USER=-57169325
 USER=$1
 SUBJECT=$2
 SUBJECT="${SUBJECT//,/ }"
+MESSAGE="chat_id=${USER}&text=$3"
 GRAPHID=$3
-ZBX_URL="http://192.168.10.24/zabbix"
+GRAPHID=$(echo $GRAPHID | grep -o -E "(Item Graphic: \[[0-9]{7}\])|(Item Graphic: \[[0-9]{6}\])|(Item Graphic: \[[0-9]{5}\])|(Item Graphic: \[[0-9]{4}\])|(Item Graphic: \[[0-9]{3}\])")
+GRAPHID=$(echo $GRAPHID | grep -o -E "([0-9]{7})|([0-9]{6})|([0-9]{5})|([0-9]{4})|([0-9]{3})")
+ZBX_URL="http://192.168.0.102/zabbix"
+ZABBIXMSG="/tmp/zabbix-message-$(date "+%Y.%m.%d-%H.%M.%S").tmp"
+
+#############################################
+# Se nao desejar enviar / grafico = 0
+#############################################
+ENVIA_GRAFICO=1
 
 ##############################################
 # Conta de usuário para logar no site Zabbix
 ##############################################
 
-USERNAME="guest"
-PASSWORD=""
+USERNAME="admin"
+PASSWORD="zabbix"
 
 ##############################################
 # Graficos
 ##############################################
 
 WIDTH=800
-COOKIE="/tmp/telegram_cookie-$(date "+%Y.%m.%d-%H.%M.%S")"
 CURL="/usr/bin/curl"
+COOKIE="/tmp/telegram_cookie-$(date "+%Y.%m.%d-%H.%M.%S")"
 PNG_PATH="/tmp/telegram_graph-$(date "+%Y.%m.%d-%H.%M.%S").png"
 
 ############################################
@@ -56,28 +65,38 @@ PERIOD=10800
 
 BOT_TOKEN='161080402:AAGah3HIxM9jUr0NX1WmEKX3cJCv9PyWD58'
 
+###########################################
+# Verifica se foi passado os 3 parametros
+# para o script
+###########################################
+
+if [ "$#" -lt 3 ]
+then
+	exit 1
+fi
 
 ############################################
 # Envio Mensagem de Texto do Alerta
 ############################################
 
-${CURL} -k -s -c ${COOKIE} -b ${COOKIE} -s -X GET "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${USER}&text=${SUBJECT}"  > /dev/null
-
+echo "$MESSAGE" > $ZABBIXMSG
+${CURL} -k -s -c ${COOKIE} -b ${COOKIE} -s -X GET "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage?chat_id=${USER}&text=\"${SUBJECT}\""  > /dev/null
+${CURL} -k -s -c ${COOKIE} -b ${COOKIE} --data-binary @${ZABBIXMSG} -X GET "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage"  > /dev/null
 
 ############################################
 # Envio dos graficos
 ############################################
 
-# Se existir valor no GRAPHID ele envia
-[ -n "$GRAPHID" ] && {
-
+# Se passou os 3 parametros para o script e ENVIA_GRAFICO=1 ele envia o gráfico.
+if [ "$#" -eq 3 ] && [ "$ENVIA_GRAFICO" -eq 1 ]
+then
 	############################################
 	# Zabbix logando com o usuário no site
 	############################################
 
-
    	# Zabbix - Ingles - Verifique no seu Zabbix se na tela de login se o botao de login é "Sign in".
 	# Obs.: Caso queira mudar, abra a configuração do usuário Guest e mude a linguagem para Portugues, se fizer isso comente (#) a linha abaixo e descomente a linha Zabbix-Portugues.
+
     	${CURL} -k -s -c ${COOKIE} -b ${COOKIE} -d "name=${USERNAME}&password=${PASSWORD}&autologin=1&enter=Sign%20in" ${ZBX_URL}"/index.php" > /dev/null
 
     	# Zabbix - Portugues - Verifique no seu Zabbix se na tela de login se o botao de login é  "Conectar-se".
@@ -85,20 +104,22 @@ ${CURL} -k -s -c ${COOKIE} -b ${COOKIE} -s -X GET "https://api.telegram.org/bot$
 
 	# Download do gráfico e envio
 	${CURL} -k -s -c ${COOKIE}  -b ${COOKIE} -d "itemids=${GRAPHID}&period=${PERIOD}&width=${WIDTH}" ${ZBX_URL}"/chart.php" -o "${PNG_PATH}"
+
 	${CURL} -k -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendPhoto" -F chat_id="${USER}" -F photo="@${PNG_PATH}" > /dev/null
-}
+
+fi
 
 ############################################
 # DEBUG
 ############################################
 
 # Verificar valores recebidos do Zabbix ou do prompt
-# echo "User-Telegram=$USER | Subject=$SUBJECT | GraphID=${GRAPHID} | Period=${PERIOD} | Width=${WIDTH}" >/tmp/telegram-debug.txt
 # cat /tmp/telegram-debug.txt
+# echo "User-Telegram=$USER | Subject=$SUBJECT | Menssage=$MESSAGE | GraphID=${GRAPHID} | Period=${PERIOD} | Width=${WIDTH}" >/tmp/telegram-debug.txt
 
 # Teste com curl tentando baixar o gráfico
 # Verifique o arquivo /tmp/telegram-graph.png no seu computador para ver se o grafico esta sendo gerado corretamente
-#${CURL} -k -c ${COOKIE}  -b ${COOKIE} -d "graphid=1459&itemids=1459&period=10800&width=800" 192.168.10.24/zabbix/chart.php > /tmp/telegram-graph.png
+# ${CURL} -k -c ${COOKIE}  -b ${COOKIE} -d "graphid=1459&itemids=1459&period=10800&width=800" 192.168.10.24/zabbix/chart.php > /tmp/telegram-graph.png
 
 #Verificando o envio da msg
 
@@ -117,6 +138,5 @@ ${CURL} -k -s -c ${COOKIE} -b ${COOKIE} -s -X GET "https://api.telegram.org/bot$
 
 rm -f ${COOKIE}
 rm -f ${PNG_PATH}
-
+rm -f ${ZABBIXMSG}
 exit 0
-
