28dffe5c4d93db1cf739294579504cce65ca9976
Diego Maia <diegosantosmaia@gmail.com>
2017-10-25 10:05:52 -0200

    Zabbix 3.4.1 Update

--- zabbix-telegram.sh
+++ zabbix-telegram.sh
@@ -3,8 +3,8 @@
 ##########################################################################
 # Zabbix-Telegram envio de alerta por Telegram com graficos dos eventos
 # Filename: zabbix-telegram.sh
-# Revision: 2.2
-# Date: 07/02/2017
+# Revision: 2.3
+# Date: 25/10/2017
 # Author: Diego Maia - diegosmaia@yahoo.com.br Telegram - @diegosmaia
 # Aproveitei algumas coisas:
 # Script getItemGraph.sh Author: Qicheng
@@ -36,11 +36,26 @@ ZBX_URL="http://192.168.0.102/zabbix"
 USERNAME="admin"
 PASSWORD="zabbix"
 
+#############################################
+# Zabbix Versao >= 3.4.1
+# 0 para nao e 1 para sim
+#############################################
+
+ZABBIXVERSION34="0"
+
 ############################################
 # O Bot-Token do exemplo, tem que modificar
 ############################################
 
 BOT_TOKEN='161080402:AAGah3HIxM9jUr0NX1WmEKX3cJCv9PyWD58'
+
+
+# Se não receber o valor de GRAPHID ele seta o valor de ENVIA_GRAFICO para 0
+
+case $GRAPHID in
+	''|*[!0-9]*) ENVIA_GRAFICO=0 ;;
+	*) ENVIA_GRAFICO=1 ;;
+esac
 
 #############################################
 # Se nao desejar enviar GRAFICO / ENVIA_GRAFICO = 0
@@ -49,13 +64,6 @@ BOT_TOKEN='161080402:AAGah3HIxM9jUr0NX1WmEKX3cJCv9PyWD58'
 
 ENVIA_GRAFICO=1
 ENVIA_MESSAGE=1
-
-# Se não receber o valor de GRAPHID ele seta o valor de ENVIA_GRAFICO para 0
-
-case $GRAPHID in
-    ''|*[!0-9]*) ENVIA_GRAFICO=0 ;;
-    *) ENVIA_GRAFICO=1 ;;
-esac
 
 
 ##############################################
@@ -105,29 +113,33 @@ if [ $(($ENVIA_GRAFICO)) -eq '1' ]; then
 	# Zabbix logando com o usuário no site
 	############################################
 
-   	# Zabbix - Ingles - Verifique no seu Zabbix se na tela de login se o botao de login é "Sign in".
+	# Zabbix - Ingles - Verifique no seu Zabbix se na tela de login se o botao de login é "Sign in".
 	# Obs.: Caso queira mudar, abra a configuração do usuário Guest e mude a linguagem para Portugues, se fizer isso comente (#) a linha abaixo e descomente a linha Zabbix-Portugues.
 
-    	${CURL} -k -s -c ${COOKIE} -b ${COOKIE} -d "name=${USERNAME}&password=${PASSWORD}&autologin=1&enter=Sign%20in" ${ZBX_URL}"/index.php" > /dev/null
+		${CURL} -k -s -c ${COOKIE} -b ${COOKIE} -d "name=${USERNAME}&password=${PASSWORD}&autologin=1&enter=Sign%20in" ${ZBX_URL}"/index.php" > /dev/null
 
-    	# Zabbix - Portugues - Verifique no seu Zabbix se na tela de login se o botao de login é  "Conectar-se".
-    	 #${CURL} -k -s -c ${COOKIE} -b ${COOKIE} -d "name=${USERNAME}&password=${PASSWORD}&autologin=1&enter=Conectar-se" ${ZBX_URL}"/index.php" > /dev/null
+		# Zabbix - Portugues - Verifique no seu Zabbix se na tela de login se o botao de login é  "Conectar-se".
+		 #${CURL} -k -s -c ${COOKIE} -b ${COOKIE} -d "name=${USERNAME}&password=${PASSWORD}&autologin=1&enter=Conectar-se" ${ZBX_URL}"/index.php" > /dev/null
 
 	# Para enviar em alguns caso ao inves do grafico do lastdata um grafico personalizado
 	# No zabbix em Graphs verifica no endereço o graphid e na mensagem que vc recebe com o gráfico a ser mudado o Item Graphic
 	# O Deny Codignotto que solicitou ajuda neste item
 	if [ "${GRAPHID}" == "000001" ]; then
 		GRAPHID="00002";
-    	${CURL} -k -s -c ${COOKIE} -b ${COOKIE} -d "graphid=${GRAPHID}&period=${PERIOD}&width=${WIDTH}" ${ZBX_URL}"/chart2.php" -o "${PNG_PATH}";
-  	elif [ "${GRAPHID}" == "000002" ]; then
-  		GRAPHID="00003";
-	    ${CURL} -k -s -c ${COOKIE}  -b ${COOKIE} -d "graphid=${GRAPHID}&period=${PERIOD}&width=${WIDTH}" ${ZBX_URL}"/chart2.php" -o "${PNG_PATH}";
+		${CURL} -k -s -c ${COOKIE} -b ${COOKIE} -d "graphid=${GRAPHID}&period=${PERIOD}&width=${WIDTH}" ${ZBX_URL}"/chart2.php" -o "${PNG_PATH}";
+	elif [ "${GRAPHID}" == "000002" ]; then
+		GRAPHID="00003";
+		${CURL} -k -s -c ${COOKIE}  -b ${COOKIE} -d "graphid=${GRAPHID}&period=${PERIOD}&width=${WIDTH}" ${ZBX_URL}"/chart2.php" -o "${PNG_PATH}";
 	elif [ "${GRAPHID}" == "000003" ]; then
 		GRAPHID="00004";
-	    ${CURL} -k -s -c ${COOKIE}  -b ${COOKIE} -d "graphid=${GRAPHID}&period=${PERIOD}&width=${WIDTH}" ${ZBX_URL}"/chart2.php" -o "${PNG_PATH}";
-  	else
-  		# Download do gráfico e envio
-		${CURL} -k -s -c ${COOKIE}  -b ${COOKIE} -d "itemids=${GRAPHID}&period=${PERIOD}&width=${WIDTH}" ${ZBX_URL}"/chart.php" -o "${PNG_PATH}";
+		${CURL} -k -s -c ${COOKIE}  -b ${COOKIE} -d "graphid=${GRAPHID}&period=${PERIOD}&width=${WIDTH}" ${ZBX_URL}"/chart2.php" -o "${PNG_PATH}";
+	else
+		# Download do gráfico e envio
+		if [ "${ZABBIXVERSION34}" == "1" ]; then
+			${CURL} -k -s -c ${COOKIE}  -b ${COOKIE} -d "itemids=${GRAPHID}&period=${PERIOD}&width=${WIDTH}&profileIdx=web.item.graph" ${ZBX_URL}"/chart.php" -o "${PNG_PATH}";
+		else
+			${CURL} -k -s -c ${COOKIE}  -b ${COOKIE} -d "itemids=${GRAPHID}&period=${PERIOD}&width=${WIDTH}" ${ZBX_URL}"/chart.php" -o "${PNG_PATH}";
+		fi
 	fi
 
 	${CURL} -k -s -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendPhoto" -F chat_id="${USER}" -F photo="@${PNG_PATH}" > /dev/null
